87f22b78e91e84b08375580b9be5edadb1493b54
ressay <bourahla.yasser@gmail.com>
2019-04-30 12:19:07 +0100

    removing masking and padding

--- DialogueManager/agent.py
+++ DialogueManager/agent.py
@@ -298,7 +298,7 @@ class Agent(object):
             st, tr, ac = state
         else:
             tr, ac = state
-        pair = len(tr)#, len(ac)
+        pair = len(tr), len(ac)
         if pair not in self.memory_map:
             self.memory_map[pair] = {}
             self.pair_count[pair] = 0
@@ -435,8 +435,8 @@ class Agent(object):
                 targets = pad_sequences(targets)
             else:
                 input_tr = np.array(input_tr)
-                input_ac = pad_sequences(input_ac, value=self.maskv)
-                targets = pad_sequences(targets, value=self.maskv)
+                # input_ac = pad_sequences(input_ac, value=self.maskv)
+                # targets = pad_sequences(targets, value=self.maskv)
             self.samples_trained += len(targets)
             self.avg_triplets_sample += len(input_tr[0])*len(targets)
             yield [input_tr, input_st, input_ac], targets
